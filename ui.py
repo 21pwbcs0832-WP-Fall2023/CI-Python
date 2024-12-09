c083475d4c2be99f9f12c2a7ab46b15799887b66
21pwbcs0832-WP-Fall2023 <sajjadafridi6913@gmail.com>
2024-12-09 13:15:15 +0500

    Project Initiated

---
+++ ui.py
@@ -0,0 +1,44 @@
+import gradio as gr
+from example1 import chain
+
+# Define the function for handling chat
+def chat(question, history):
+    if question == "":
+        response = "Please ask a question"
+        history.append({"role": "assistant", "content": response})
+    else:
+        response = chain.stream(question)
+        history.append({"role": "assistant", "content": ""})
+
+        for i in response:
+            history[-1]['content'] += i
+            yield "", history
+
+# Create the Gradio interface
+with gr.Blocks(title="Chat with GPT-4") as demo:
+    gr.Markdown("# Chat with GPT-4 DEMO")
+    with gr.Row():
+        gr.Markdown("")
+    with gr.Column(scale=6):
+        chatbox = gr.Chatbot(type="messages")
+        with gr.Row():
+            textbox = gr.Textbox(
+                scale=7, container=False, placeholder="Ask a question"
+            )
+            submit_button = gr.Button(value="Submit", scale=3, variant="primary")
+    gr.Markdown("")
+
+    # Define the event listeners inside the `with gr.Blocks` context
+    textbox.submit(chat, inputs=[textbox, chatbox], outputs=[textbox, chatbox])
+    submit_button.click(chat, inputs=[textbox, chatbox], outputs=[textbox, chatbox])
+
+# Launch the Gradio interface
+demo.launch()
+
+
+# gr.ChatInterface (
+#     fn = chat,
+#     type = "messages"
+# ).launch(
+#     share = True,
+# )
